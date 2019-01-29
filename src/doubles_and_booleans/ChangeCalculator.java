fb98d23dfac64ab4f62dab046bf384342a8acb07
League <league@WTS9.attlocal.net>
2019-01-28 16:55:55 -0800

    1/28/19

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,23 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+String name =JOptionPane.showInputDialog("Dear user, what is your name?");
+JOptionPane.showMessageDialog(null,"Okay, " +name+ ". Have you ever had a pocket full of change? Do you hate doing math. Well, this is a program that calcucates your change without your brain actually doing any work!! Yippie!!");
+String nickels = JOptionPane.showInputDialog("Come on, " +name+ ". Lets stop beating about the bush. First question : How many nickels do you have. Enter your answer in the box below.");
 		// Convert their answer to an int using Integer.parseInt()
-
+int nic = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
+String dimes = JOptionPane.showInputDialog("Next question, " +name+ ". How many dimes to you have? Enter your answer into the box below.");
+int dime = Integer.parseInt(dimes);
 
 		// Ask the user how many quarters they have, and convert their answer
+String quarters = JOptionPane.showInputDialog("Final question, " +name+ ". How many quarters do you have. Enter your answer in the box below.");
+int quarter = Integer.parseInt(quarters);
 
-		// Calculate how much money the user has and save it in a double variable 
-
+		// Calculate how much money the user has and save it in a double variable
+double money = (nic * 0.05)+(dime * 0.1)+(quarter * 0.25) ;
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "We are done! You have " +money+ " dollars, " +name+ "!! Have a good rest of your day!!");
 	}
 }
 
---
+++ src/doubles_and_booleans/Testscore.java
@@ -0,0 +1,19 @@
+package doubles_and_booleans;
+
+import javax.swing.JOptionPane;
+
+public class Testscore {
+public static void main(String[] args) {
+	
+	String name = JOptionPane.showInputDialog("Dear user, what is your name?");
+	String testscore = JOptionPane.showInputDialog("Okay, " +name+ ". Now, if you don't mind, what is your most recent test score?");
+Double.parseDouble(testscore);
+	
+	
+	
+	
+	
+	
+	
+}
+}
